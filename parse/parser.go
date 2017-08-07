8504a1c45d5bfe59827771442753f7f638b52072
Thomas Hallgren <thomas@tada.se>
2017-08-07 10:02:29 +0200

    Move to go-parser sub package

--- parse/parser.go
+++ parse/parser.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "os"
-  "fmt"
+  . "os"
+  . "fmt"
+  . "github.com/puppetlabs/go-parser/parser"
+  . "strings"
   "io/ioutil"
+  "flag"
   "encoding/json"
   "bytes"
-  . "github.com/puppetlabs/parser"
-  "strings"
-  "flag"
 )
 
 var validateOnly = flag.Bool("v", false, "validate only")
@@ -18,8 +18,8 @@ func main() {
 
   args := flag.Args()
   if len(args) != 1 {
-    fmt.Fprintln(os.Stderr, "usage: parser [-v] <pp or epp file to parse>")
-    os.Exit(1)
+    Fprintln(Stderr, "usage: parser [-v] <pp or epp file to parse>")
+    Exit(1)
   }
 
   fileName := args[0]
@@ -28,19 +28,19 @@ func main() {
     panic(err)
   }
 
-  expr, err := Parse(args[0], string(content), strings.HasSuffix(fileName, `.epp`))
+  expr, err := Parse(args[0], string(content), HasSuffix(fileName, `.epp`))
   if err != nil {
-    fmt.Fprintln(os.Stderr, err.Error())
-    os.Exit(1)
+    Fprintln(Stderr, err.Error())
+    Exit(1)
   }
 
   v := NewValidator()
   v.Validate(expr)
   if len(v.Issues()) > 0 {
     for _, issue := range v.Issues() {
-      fmt.Fprintln(os.Stderr, issue.String())
+      Fprintln(Stderr, issue.String())
     }
-    os.Exit(1)
+    Exit(1)
   }
 
   if !*validateOnly {
@@ -49,6 +49,6 @@ func main() {
     enc.SetEscapeHTML(false)
     enc.Encode(expr.ToPN().ToData())
     result.Truncate(result.Len() - 1)
-    fmt.Println(result.String())
+    Println(result.String())
   }
 }
--- parser/validate_test.go
+++ parser/validate_test.go
@@ -9,37 +9,37 @@ func TestVariableAssignValidation(t *testing.T) {
 }
 
 func TestNumericVariableAssignValidation(t *testing.T) {
-  expectIssues(t, `$1 = 'y'`, ILLEGAL_NUMERIC_ASSIGNMENT)
+  expectIssues(t, `$1 = 'y'`, VALIDATE_ILLEGAL_NUMERIC_ASSIGNMENT)
 }
 
 func TestMultipleVariableAssign(t *testing.T) {
   expectNoIssues(t, `[$a, $b] = 'y'`)
-  expectIssues(t, `[$a, $1] = 'y'`, ILLEGAL_NUMERIC_ASSIGNMENT)
-  expectIssues(t, `[$a, $b['h']] = 'y'`, ILLEGAL_ASSIGNMENT_VIA_INDEX)
-  expectIssues(t, `[$a, $b::z] = 'y'`, CROSS_SCOPE_ASSIGNMENT)
+  expectIssues(t, `[$a, $1] = 'y'`, VALIDATE_ILLEGAL_NUMERIC_ASSIGNMENT)
+  expectIssues(t, `[$a, $b['h']] = 'y'`, VALIDATE_ILLEGAL_ASSIGNMENT_VIA_INDEX)
+  expectIssues(t, `[$a, $b::z] = 'y'`, VALIDATE_CROSS_SCOPE_ASSIGNMENT)
 }
 
 func TestAccessAssignValidation(t *testing.T) {
-  expectIssues(t, `$x['h'] = 'y'`, ILLEGAL_ASSIGNMENT_VIA_INDEX)
+  expectIssues(t, `$x['h'] = 'y'`, VALIDATE_ILLEGAL_ASSIGNMENT_VIA_INDEX)
 }
 
 func TestAppendsDeletesValidation(t *testing.T) {
-  expectIssues(t, `$x += 'y'`, APPENDS_DELETES_NO_LONGER_SUPPORTED)
-  expectIssues(t, `$x -= 'y'`, APPENDS_DELETES_NO_LONGER_SUPPORTED)
+  expectIssues(t, `$x += 'y'`, VALIDATE_APPENDS_DELETES_NO_LONGER_SUPPORTED)
+  expectIssues(t, `$x -= 'y'`, VALIDATE_APPENDS_DELETES_NO_LONGER_SUPPORTED)
 }
 
 func TestNamespaceAssignValidation(t *testing.T) {
-  expectIssues(t, `$x::z = 'y'`, CROSS_SCOPE_ASSIGNMENT)
+  expectIssues(t, `$x::z = 'y'`, VALIDATE_CROSS_SCOPE_ASSIGNMENT)
 }
 
 func TestAttributeAppendValidation(t *testing.T) {
   expectNoIssues(t, `Service[apache] { require +> File['apache.pem'] }`)
 
-  expectIssues(t, `service { apache: require +> File['apache.pem'] }`, ILLEGAL_ATTRIBUTE_APPEND)
+  expectIssues(t, `service { apache: require +> File['apache.pem'] }`, VALIDATE_ILLEGAL_ATTRIBUTE_APPEND)
 }
 
 func TestBinaryOpValidation(t *testing.T) {
-  expectIssues(t, `notice(function foo() {} < 3)`, NOT_RVALUE)
+  expectIssues(t, `notice(function foo() {} < 3)`, VALIDATE_NOT_RVALUE)
   expectNoIssues(t, `notice(true == !false)`)
 }
 
@@ -49,7 +49,7 @@ func TestBlockValidation(t *testing.T) {
       ['a', 'b']
       $x = 3
       `),
-    IDEM_EXPRESSION_NOT_LAST)
+    VALIDATE_IDEM_EXPRESSION_NOT_LAST)
 }
 
 func expectNoIssues(t *testing.T, str string) {
